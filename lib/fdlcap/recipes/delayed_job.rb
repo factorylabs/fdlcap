982aa6cecb94622fae1ad26fc527c35db9e0f96c
Jay Zeschin <jay.zeschin@factorylabs.com>
2009-06-25 22:34:01 -0600

    Added hooks for start/stop/restart for delayed_job

--- lib/fdlcap/recipes/delayed_job.rb
+++ lib/fdlcap/recipes/delayed_job.rb
@@ -1,16 +1,22 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  namespace :delayed_job do
-    desc "Start delayed_job"
-    task :start, :only => {:delayed_job => true} do
-      sudo "/usr/bin/monit start all -g dj_#{application}"
+  define_recipe :delayed_job do
+    namespace :delayed_job do
+      desc "Start delayed_job"
+      task :start, :only => {:delayed_job => true} do
+        sudo "/usr/bin/monit start all -g dj_#{application}"
+      end
+      desc "Stop delayed_job"
+      task :stop, :only => {:delayed_job => true} do
+        sudo "/usr/bin/monit stop all -g dj_#{application}"
+      end
+      desc "Restart delayed_job"
+      task :restart, :only => {:delayed_job => true} do
+        sudo "/usr/bin/monit restart all -g dj_#{application}"
+      end        
     end
-    desc "Stop delayed_job"
-    task :stop, :only => {:delayed_job => true} do
-      sudo "/usr/bin/monit stop all -g dj_#{application}"
-    end
-    desc "Restart delayed_job"
-    task :restart, :only => {:delayed_job => true} do
-      sudo "/usr/bin/monit restart all -g dj_#{application}"
-    end        
+  
+    after "deploy:restart", "delayed_job:restart"
+    after "deploy:start",   "delayed_job:start"
+    after "deploy:stop",    "delayed_job:stop"
   end
 end
