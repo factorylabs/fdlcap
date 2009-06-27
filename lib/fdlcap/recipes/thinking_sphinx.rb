ff3b1983d529056ab327c1e9f9c7aa4154689014
Gabe Varela <gvarela@gmail.com>
2009-06-26 22:25:59 -0600

    added separate ts setups based on different ey setups, one for searchd and the other for thinking_sphinx rake tasks

--- lib/fdlcap/recipes/thinking_sphinx.rb
+++ lib/fdlcap/recipes/thinking_sphinx.rb
@@ -1,8 +1,19 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  define_recipe :thinking_sphinx do
+  # These recipies assume an engineyard configuration
+  
+  # searchd assumes indexing and configuring being called directly on searchd
+  define_recipe :thinking_sphinx_searchd do
     after "deploy:update_code",       "deploy:symlink_configs"
     after "deploy:symlink_configs",   "thinking_sphinx:symlink"
     after "thinking_sphinx:symlink",  "sphinx:configure"
     after "deploy:update",            "sphinx:reindex"
+  end
+  
+  # rake assumes the explicit ey cap tasks that call TS rake tasks for indexing and configuring
+  define_recipe :thinking_sphinx_rake do
+    after "deploy:update_code",       "deploy:symlink_configs"
+    after "deploy:symlink_configs",   "thinking_sphinx:symlink"
+    after "thinking_sphinx:symlink",  "thinking_sphinx:configure"
+    after "deploy:update",            "thinking_sphinx:reindex"
   end
 end
