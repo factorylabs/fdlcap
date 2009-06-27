29ee65d49219ee86d65d418d9d2b51b51efa3093
Gabe Varela <gvarela@gmail.com>
2009-06-26 23:11:57 -0600

    change what is in defaults

--- lib/fdlcap/defaults.rb
+++ lib/fdlcap/defaults.rb
@@ -1,5 +1,8 @@
 Capistrano::Configuration.instance(:must_exist).load do
   
-  
+  # defaults for fdl
+  set :scm,           :git
+  set :keep_releases, 5
+  set :deploy_via,    :remote_cache
   
 end
