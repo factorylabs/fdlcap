6cffb4d764792838a64d9a8226f45363d3842c1a
Jay Zeschin <jay.zeschin@factorylabs.com>
2009-06-29 11:08:38 -0600

    Added -p option to mkdir

--- lib/fdlcap/recipes/ruby_inline.rb
+++ lib/fdlcap/recipes/ruby_inline.rb
@@ -5,8 +5,8 @@ Capistrano::Configuration.instance(:must_exist).load do
     # Tasks
     #
     task :ruby_inline, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
-      run "mkdir #{release_path}/tmp/ruby_inline/"
-      run "mkdir #{release_path}/tmp/ruby_inline/.ruby_inline"
+      run "mkdir -p #{release_path}/tmp/ruby_inline/"
+      run "mkdir -p #{release_path}/tmp/ruby_inline/.ruby_inline"
       sudo "chmod 755 #{release_path}/tmp/ruby_inline/"
       sudo "chmod 755 #{release_path}/tmp/ruby_inline/.ruby_inline"
     end
