fbbc8451dd4c0332b1236c06ae438f46e1473b98
Jay Zeschin <jay.zeschin@factorylabs.com>
2009-06-29 11:07:31 -0600

    Changed requires to automatically pull in the whole recipes directory

--- lib/fdlcap/recipes.rb
+++ lib/fdlcap/recipes.rb
@@ -6,23 +6,6 @@ require 'fdlcap/extensions/recipe_definition'
 require 'eycap/recipes'
 
 # Load up custom recipe chunks
-require 'fdlcap/recipes/autotagger'
-require 'fdlcap/recipes/craken'
-require 'fdlcap/recipes/database'
-require 'fdlcap/recipes/delayed_job'
-require 'fdlcap/recipes/deploy'
-require 'fdlcap/recipes/fdl_defaults'
-require 'fdlcap/recipes/geminstaller'
-require 'fdlcap/recipes/newrelic'
-require 'fdlcap/recipes/nginx'
-require 'fdlcap/recipes/performance'
-require 'fdlcap/recipes/rake'
-require 'fdlcap/recipes/rsync'
-require 'fdlcap/recipes/ruby_inline'
-require 'fdlcap/recipes/sass'
-require 'fdlcap/recipes/slice'
-require 'fdlcap/recipes/ssh'
-require 'fdlcap/recipes/stages'
-require 'fdlcap/recipes/symlinks'
-require 'fdlcap/recipes/thin'
-require 'fdlcap/recipes/thinking_sphinx'
+Dir.glob(File.join(File.dirname(__FILE__),'recipes','*.rb')).each do |file|
+  require file
+end
