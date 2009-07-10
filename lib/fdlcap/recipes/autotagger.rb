41b2ea3c5f1930c99a6e99d3c51a4721586207da
Gabe Varela <gvarela@gmail.com>
2009-07-09 23:42:27 -0600

    reorder callbacks for autotagger

--- lib/fdlcap/recipes/autotagger.rb
+++ lib/fdlcap/recipes/autotagger.rb
@@ -32,7 +32,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     before  "deploy:update_code",      "release_tagger:set_branch"
     before  "deploy:cleanup",          "release_tagger:create_tag"
     before  "deploy:cleanup",          "release_tagger:write_tag_to_shared"
-    before  "deploy:cleanup",          "release_tagger:print_latest_tags"
     before  "deploy:cleanup",          "release_tagger:remove_previous_tags"
+    before  "deploy:cleanup",          "release_tagger:print_latest_tags"
   end
 end
