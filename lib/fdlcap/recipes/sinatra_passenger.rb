c5c30748f2c5163b80921eb1c96c97a4f6abb6ca
jsmestad <justin.smestad@gmail.com>
2009-08-24 10:34:25 -0600

    fix \\ error; should be \;"

--- lib/fdlcap/recipes/sinatra_passenger.rb
+++ lib/fdlcap/recipes/sinatra_passenger.rb
@@ -3,12 +3,12 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :sinatra_passenger do
       desc "Set file permissions"
       task :chmod_directories_config do
-        run "cd #{release_path} && find -maxdepth 1 -type d -exec chmod 755 {} \\"
+        run "cd #{release_path} && find -maxdepth 1 -type d -exec chmod 755 {} \;"
       end
 
       desc "Set public folder permissions"
       task :chmod_directories_public do
-        run "cd #{release_path}/public && find -type d -exec chmod 755 {} \\"
+        run "cd #{release_path}/public && find -type d -exec chmod 755 {} \;"
       end
     end
 
