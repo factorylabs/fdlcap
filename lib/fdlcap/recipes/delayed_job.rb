2fa49c1f7b52a6fa3001940244cef89ca2fa9e17
Gabe Varela <gvarela@gmail.com>
2009-09-22 10:32:37 -0600

    allow configuration of monit process prefix for delayed_job

--- lib/fdlcap/recipes/delayed_job.rb
+++ lib/fdlcap/recipes/delayed_job.rb
@@ -1,17 +1,23 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  define_recipe :delayed_job do
+  define_recipe :delayed_job do |*args|
+
+    options = args.empty? ? {} : args.first
+    prefix = options[:prefix] || 'dj'
+    set :dj_monit_prefix, prefix unless exists?(:dj_monit_prefix)
+    
     namespace :delayed_job do
+
       desc "Start delayed_job"
       task :start, :only => {:delayed_job => true} do
-        sudo "/usr/bin/monit start all -g dj_#{application}"
+        sudo "/usr/bin/monit start all -g #{dj_monit_prefix}_#{application}"
       end
       desc "Stop delayed_job"
       task :stop, :only => {:delayed_job => true} do
-        sudo "/usr/bin/monit stop all -g dj_#{application}"
+        sudo "/usr/bin/monit stop all -g #{dj_monit_prefix}_#{application}"
       end
       desc "Restart delayed_job"
       task :restart, :only => {:delayed_job => true} do
-        sudo "/usr/bin/monit restart all -g dj_#{application}"
+        sudo "/usr/bin/monit restart all -g #{dj_monit_prefix}_#{application}"
       end        
     end
   
