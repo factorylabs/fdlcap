6ac75ace58453f8e5590af190f85987d0d8f0b35
jsmestad <justin.smestad@gmail.com>
2010-01-11 15:42:39 -0700

    remove geminstaller from fdl_defaults, update version

--- lib/fdlcap/recipes/fdl_defaults.rb
+++ lib/fdlcap/recipes/fdl_defaults.rb
@@ -7,7 +7,6 @@ Capistrano::Configuration.instance(:must_exist).load do
     set :deploy_via,    :remote_cache
     
     use_recipe :symlinks
-    use_recipe :geminstaller
     use_recipe :perform_cleanup
     use_recipe :symlink_configs
     use_recipe :automigrate
