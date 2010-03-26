c4199a7ff42d922573373356ce52c8f10c6fcb54
Jay Zeschin <jay.zeschin@factorylabs.com>
2010-03-26 11:19:18 -0600

    Copy maintenance page task needs to be in the slice dir

--- lib/fdlcap/recipes/slice.rb
+++ lib/fdlcap/recipes/slice.rb
@@ -48,9 +48,11 @@ Capistrano::Configuration.instance(:must_exist).load do
   # Deploy the custom maintenance page
   define_recipe :custom_maintenance_page do
     
-    desc "Copy the maintenance page from the public directory to the shared directory"
-    task :copy_maintenance_page, :roles => :app do
-      upload "public/maintenance.html","#{shared_path}/system/maintenance.html.custom", :via => :scp
+    namespace :slice do
+      desc "Copy the maintenance page from the public directory to the shared directory"
+      task :copy_maintenance_page, :roles => :app do
+        upload "public/maintenance.html","#{shared_path}/system/maintenance.html.custom", :via => :scp
+      end
     end
     
     before "deploy:web:disable",      "slice:copy_maintenance_page"
