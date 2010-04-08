b1d5767f2e58a23f919b4d6195f109a732fa92d8
Jay Zeschin & Mike Barinek <pair+jzeschin+mbarinek@pivotallabs.com>
2010-04-08 15:41:04 -0600

    Make thinking_sphinx:symlink always run on deploy (not conditional on DB changes)

--- lib/fdlcap/recipes/thinking_sphinx.rb
+++ lib/fdlcap/recipes/thinking_sphinx.rb
@@ -3,10 +3,14 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   # searchd assumes indexing and configuring being called directly on searchd
   define_recipe :thinking_sphinx do
+    
+    #ALWAYS need to do this
+    after "deploy:symlink_configs",   "thinking_sphinx:symlink"
+    
+    # Only need to do this if DB has changed
     after 'deploy:finalize_update' do
       look_for_db_changes
       if db_changed?
-        after "deploy:symlink_configs",   "thinking_sphinx:symlink"
         after "deploy:migrate",           "sphinx:configure"
         after "deploy:migrate",           "sphinx:reindex"
       end
