3d64059b4c6ef68ac4ed19378e01362119685740
Jay Zeschin & Mike Barinek <pair+jzeschin+mbarinek@pivotallabs.com>
2010-04-09 10:07:38 -0600

    Refactored ruby_inline symlink recipe and added a workaround to chmod 755 the current symlink

--- lib/fdlcap/recipes/ruby_inline.rb
+++ lib/fdlcap/recipes/ruby_inline.rb
@@ -5,15 +5,20 @@ Capistrano::Configuration.instance(:must_exist).load do
     # Tasks
     #
     task :ruby_inline, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
-      run "mkdir -p #{release_path}/tmp/ruby_inline/"
       run "mkdir -p #{release_path}/tmp/ruby_inline/.ruby_inline"
-      sudo "chmod 755 #{release_path}/tmp/ruby_inline/"
-      sudo "chmod 755 #{release_path}/tmp/ruby_inline/.ruby_inline"
+      run "chmod -R 755 #{release_path}/tmp/ruby_inline/"
+    end
+    
+    # todo - revisit this, currently a hack to temporarily fix an issue with sphinx and ruby_inline
+    task :chmod_release_path, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
+      run "chmod 755 #{release_path}"
     end
     
     #
     # Callbacks
     #
     after "deploy:symlink_configs", "ruby_inline"
+    after "deploy:symlink",         "chmod_release_path"
+    
   end
 end
